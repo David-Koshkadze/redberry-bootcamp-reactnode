560b82ce31f5330dfc6027d2a9fc40393ef08955
David-Koshkadze <rootUS3rr@pm.me>
2022-08-27 13:27:29 +0400

    add styles

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -6,13 +6,13 @@ const InfoPage = () => {
           <div className="input-flex">
             <label htmlFor="first_name">სახელი</label>
             <input type="text" id="first_name" name="first_name" placeholder="გრიშა"/>
-            <span>მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
+            <span className="text-hint">მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
           </div>
 
           <div className="input-flex">
             <label htmlFor="last_name">გვარი</label>
             <input type="text" id="last_name" name="last_name" placeholder="ბაგრატიონი"/>
-            <span>მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
+            <span className="text-hint">მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
           </div>
         </div>
 
@@ -27,15 +27,16 @@ const InfoPage = () => {
         <div className="input-flex">
           <label htmlFor="email">მეილი</label>
           <input type="text" id="email" name="email" placeholder="grish666@redberry.ge"/>
-          <span>უნდა მთავრდებოდეს @redberry.ge-ით</span>
+          <span className="text-hint">უნდა მთავრდებოდეს @redberry.ge-ით</span>
         </div>
 
         <div className="input-flex">
           <label htmlFor="phone_number">ტელეფონის ნომერი</label>
           <input type="text" id="phone_number" name="phone_number" placeholder="+995 598 00 07 01"/>
-          <span className="text-info">უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს</span>
+          <span className="text-hint">უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს</span>
         </div>
 
+        <button className="btn btn-blue" style={{marginLeft: 'auto', marginTop: '5rem'}}>შემდეგი</button>
       </div>
     </>
   );
